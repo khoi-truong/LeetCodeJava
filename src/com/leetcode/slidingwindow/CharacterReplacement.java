c1a056f33c692ceb0baae7017be7cf529ccaeda4
Khoi Truong <khoi.truongminh@gmail.com>
2022-02-23 23:27:34 +0700

    feat: longest substring with the same letters after replacement

---
+++ src/com/leetcode/slidingwindow/CharacterReplacement.java
@@ -0,0 +1,27 @@
+package com.leetcode.slidingwindow;
+
+import java.util.HashMap;
+
+public class CharacterReplacement {
+    public static int findLength(String str, int k) {
+        if (str == null || str.length() == 0 || k <= 0) {
+            throw new IllegalArgumentException();
+        }
+        int maxLength = 0;
+        int windowStart = 0;
+        int maxRepeatLetterCount = 0;
+        HashMap<Character, Integer> letterFrequencies = new HashMap<>();
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+            char rightChar = str.charAt(windowEnd);
+            letterFrequencies.put(rightChar, letterFrequencies.getOrDefault(rightChar, 0) + 1);
+            maxRepeatLetterCount = Math.max(maxRepeatLetterCount, letterFrequencies.get(rightChar));
+            while (windowEnd - windowStart + 1 - maxRepeatLetterCount > k) {
+                char leftChar = str.charAt(windowStart);
+                letterFrequencies.put(leftChar, letterFrequencies.get(leftChar) - 1);
+                windowStart++;
+            }
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+        }
+        return maxLength;
+    }
+}
---
+++ test/main/CharacterReplacementTest.java
@@ -0,0 +1,26 @@
+import com.leetcode.slidingwindow.CharacterReplacement;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CharacterReplacementTest {
+    @Test
+    void test1() {
+        assertEquals(5, CharacterReplacement.findLength("aabccbb",2));
+    }
+
+    @Test
+    void test2() {
+        assertEquals(4, CharacterReplacement.findLength("abbcb",1));
+    }
+
+    @Test
+    void test3() {
+        assertEquals(3, CharacterReplacement.findLength("abccde",1));
+    }
+
+    @Test
+    void test4() {
+        assertEquals(16, CharacterReplacement.findLength("abbbaaabcccfaaaaaaftiftaaaaaaqefsfaaaaaaarhhhhhhhhhbnchhhh",3));
+    }
+}
