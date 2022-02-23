36c5ee60b5115e79b5a32d19feaad268f6bef522
Khoi Truong <khoi.truongminh@gmail.com>
2022-02-23 22:29:27 +0700

    feat: no repeat substring

---
+++ src/com/leetcode/slidingwindow/NoRepeatSubstring.java
@@ -0,0 +1,23 @@
+package com.leetcode.slidingwindow;
+
+import java.util.HashMap;
+
+public class NoRepeatSubstring {
+    public static int findLength(String str) {
+        if (str == null || str.length() == 0) {
+            throw new IllegalArgumentException();
+        }
+        int maxLength = 0;
+        int windowStart = 0;
+        HashMap<Character, Integer> characterIndexes = new HashMap<>();
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+            char rightChar = str.charAt(windowEnd);
+            if (characterIndexes.containsKey(rightChar)) {
+                windowStart = Math.max(windowStart, characterIndexes.get(rightChar) + 1);
+            }
+            characterIndexes.put(rightChar, windowEnd);
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+        }
+        return maxLength;
+    }
+}
---
+++ test/main/NoRepeatSubstringTest.java
@@ -0,0 +1,21 @@
+import com.leetcode.slidingwindow.NoRepeatSubstring;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NoRepeatSubstringTest {
+    @Test
+    void test1() {
+        assertEquals(3, NoRepeatSubstring.findLength("aabccbb"));
+    }
+
+    @Test
+    void test2() {
+        assertEquals(2, NoRepeatSubstring.findLength("abbbb"));
+    }
+
+    @Test
+    void test3() {
+        assertEquals(3, NoRepeatSubstring.findLength("abccde"));
+    }
+}
