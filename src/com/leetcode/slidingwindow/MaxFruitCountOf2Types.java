e3c32459d74bbe9e04c385e8375b05b2c9abafda
Khoi Truong <khoi.truongminh@gmail.com>
2022-02-23 17:22:52 +0700

    feat: max fruit count of 2 types

---
+++ src/com/leetcode/slidingwindow/MaxFruitCountOf2Types.java
@@ -0,0 +1,28 @@
+package com.leetcode.slidingwindow;
+
+import java.util.HashMap;
+
+public class MaxFruitCountOf2Types {
+    public static int findLength(char[] arr) {
+        if (arr.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        int maxLength = 0;
+        int windowStart = 0;
+        HashMap<Character, Integer> characterFrequencies = new HashMap<>();
+        for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+            char rightChar = arr[windowEnd];
+            characterFrequencies.put(rightChar, characterFrequencies.getOrDefault(rightChar, 0) + 1);
+            while (characterFrequencies.size() > 2) {
+                char leftChar = arr[windowStart];
+                characterFrequencies.put(leftChar, characterFrequencies.get(leftChar) - 1);
+                if (characterFrequencies.get(leftChar) == 0) {
+                    characterFrequencies.remove(leftChar);
+                }
+                windowStart++;
+            }
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+        }
+        return maxLength;
+    }
+}
---
+++ test/main/MaxFruitCountOf2TypesTest.java
@@ -0,0 +1,16 @@
+import com.leetcode.slidingwindow.MaxFruitCountOf2Types;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MaxFruitCountOf2TypesTest {
+    @Test
+    void test1() {
+        assertEquals(3, MaxFruitCountOf2Types.findLength(new char[] { 'A', 'B', 'C', 'A', 'C' }));
+    }
+
+    @Test
+    void test2() {
+        assertEquals(5, MaxFruitCountOf2Types.findLength(new char[] { 'A', 'B', 'C', 'B', 'B', 'C' }));
+    }
+}
