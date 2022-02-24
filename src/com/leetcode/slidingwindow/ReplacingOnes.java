78a750f30607b51392df09e85327b5b113b41da3
Khoi Truong <khoi.truongminh@gmail.com>
2022-02-24 21:44:49 +0700

    feat: longest subarray with Ones after replacement

---
+++ src/com/leetcode/slidingwindow/ReplacingOnes.java
@@ -0,0 +1,30 @@
+package com.leetcode.slidingwindow;
+
+import java.util.HashMap;
+
+public class ReplacingOnes {
+    public static int findLength(int[] arr, int k) {
+        if (arr.length == 0 || k <= 0) {
+            throw new IllegalArgumentException();
+        }
+        int maxLength = 0;
+        int windowStart = 0;
+        int maxRepeatOneCount = 0;
+        HashMap<Character, Integer> letterFrequencies = new HashMap<>();
+        for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+            int rightChar = arr[windowEnd];
+            if (rightChar == 1) {
+                maxRepeatOneCount++;
+            }
+            while (windowEnd - windowStart + 1 - maxRepeatOneCount > k) {
+                int leftChar = arr[windowStart];
+                if (leftChar == 1) {
+                    maxRepeatOneCount--;
+                }
+                windowStart++;
+            }
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+        }
+        return maxLength;
+    }
+}
---
+++ test/main/ReplacingOnesTest.java
@@ -0,0 +1,16 @@
+import com.leetcode.slidingwindow.ReplacingOnes;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReplacingOnesTest {
+    @Test
+    void test1() {
+        assertEquals(6, ReplacingOnes.findLength(new int[] { 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1 },2));
+    }
+
+    @Test
+    void test2() {
+        assertEquals(9, ReplacingOnes.findLength(new int[] { 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1 },3));
+    }
+}
